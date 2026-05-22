2ca3944411fc646e6f54935f8f3f1c606af37625
kaiser1024 <kaiser.laszlo@gmail.com>
2026-05-22 18:59:34 +0300

    Update script.js

--- english/expanding-header/script.js
+++ english/expanding-header/script.js
@@ -1,5 +1,7 @@
 const topbar = document.querySelector('.topbar');
 
+const main = document.querySelector('main');
+
 let lastScroll = window.pageYOffset;
 
 window.addEventListener('scroll', () => {
@@ -7,14 +9,19 @@ window.addEventListener('scroll', () => {
 
   if (currentScroll <= 0 ) {
     topbar.classList.remove('hidden');
+
+    main.classList.remoev('no-bar');
+    return;
   }
 
   if (Math.abs(currentScroll - lastScroll) < 5) return;
 
   if (currentScroll > lastScroll) {
     topbar.classList.add('hidden');
+    main.classList.add('no-bar'); 
   } else {
     topbar.classList.remove('hidden');
+    main.classList.remove('no-bar');
   }
 
   lastScroll = currentScroll;
