02a7b4cb8666f384eedc1c4826b7a2f64d46ec5c
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-01 15:23:08 +0300

    Update app.js

--- english/contact-form/app.js
+++ english/contact-form/app.js
@@ -3,6 +3,8 @@ const inputs = document.querySelectorAll(".contact-input");
 inputs.forEach(ipt => {
   ipt.addEventListener("focus", () => {
     ipt.parentNode.classList.add("focus");
+    ipt.parentNode.classList.add("not-empty");
+
   });
 
   ipt.addEventListener("blur", () => {
